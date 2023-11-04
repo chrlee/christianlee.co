1c76da34a94d6d3b58c2806a9bba661e0b8309fd
Christian Lee <christianjslee@gmail.com>
2023-11-04 14:18:42 -0400

    Fix vercel URLs

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <footer>
     <a href="https://github.com/chrlee">
-        <img src="svg/github-mark.svg" alt="GitHub" width="24" height="24" />
+        <img src="/svg/github-mark.svg" alt="GitHub" width="24" height="24" />
     </a>
 </footer>
 
