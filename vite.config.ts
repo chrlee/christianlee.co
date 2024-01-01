a73f0ed15abdbb822c9fd39e30aef25207258b15
Christian Lee <christianjslee@gmail.com>
2023-12-31 22:45:03 -0500

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-  resolve: {
-    alias: {
-      'DRACOLoader': path.resolve('../node_modules/three/examples/jsm/loaders/DRACOLoader.js'),
-    }
+  ssr: {
+    noExternal: ['three']
   }
 });
