e8994e30c9ac85cbb89e3104d376ecc6d0af553e
Christian Lee <cllz@amazon.com>
2024-10-19 15:12:40 -0400

    Content update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,29 +9,32 @@
 <h3>Current</h3>
 
 <p>Developing shopping experiences at <a href="https://shopbop.com/" rel="nofollow">Shopbop</a>, an Amazon subsidiary focused on luxury fashion e-commerce</p> 
-<p>& working on new ways to use technology to help connect with and invest in artists.</p>
-
-<br />
 
 <h3>Previous</h3>
+<ul>
+  <li>
+    Featured for my fashion work on <a
+      href="https://www.grailed.com/drycleanonly/seller-spotlight-designer-experts">Grailed</a
+    >
+    &
+    <a
+      href="https://wwd.com/menswear-news/mens-fashion/new-york-mens-day-fall-2023-details-1235515836/"
+      >WWD</a
+    >.
+  </li>
+  <li>
+    Run an <a href="https://perlin-noise.com">online clothing store</a> centered around rare, archival menswear
+  </li>
+  <li>
+    Showcased <a href="https://noise-console.vercel.app">mathematical noise visualizations</a> in multiple dimensions
+  </li>
+  <li>
+    Re-created the <a href="https://hkr.news">HackerNews UI and API using GraphQL and Svelte</a>
+  </li>
+</ul>
 
-<p>
-  Re-designed the HackerNews UI and API using GraphQL and Svelte: <a href="https://hkr.news">hkr.news</a>
-</p>
-<p>
-  Run an online clothing store centered around rare, archival menswear: <a href="https://perlin-noise.com">perlin noise</a>
-</p>
-<p>
-  & also been featured for my fashion work on <a
-		href="https://www.grailed.com/drycleanonly/seller-spotlight-designer-experts">Grailed</a
-	>
-	&
-	<a
-		href="https://wwd.com/menswear-news/mens-fashion/new-york-mens-day-fall-2023-details-1235515836/"
-		>WWD</a
-	>.
-</p>
 <br />
+
 <p>
 	If you'd like to get in touch, feel free to <a href="mailto:christianjslee@gmail.com">reach out</a
 	>.
@@ -50,4 +53,8 @@
      color: #00E;
     }
 	}
+
+  li {
+    margin-bottom: 0.5rem;
+  }
 </style>
